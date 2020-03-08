866fa6460eff4b67ff15c7309a1e9edee1bb9463
Fatih Özcan <58841522+fatihozcan1461@users.noreply.github.com>
2020-03-08 21:09:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/runnable_ornek/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.runnable_ornek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    int number;
+    Runnable runnable;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        textView =findViewById( R.id.textView );
+        number = 0;
+    }
+
+
+    public void start(View view)
+    {
+        handler  = new Handler(  );
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                textView.setText( "Timer:"+number );
+                number++;
+                textView.setText( "Timer :" +number);
+                handler.postDelayed( runnable,1000 );
+
+            }
+        };
+        handler.post(runnable);
+
+    }
+
+    public void stop(View view)
+    {
+
+    }
+}
